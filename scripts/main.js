378d71021175efbd24825e36760a9c7d2943ffbf
LouisGAUTHIER2 <louis.gauthier3@edu.devinci.fr>
2026-05-16 23:02:30 +0200

    Correction shader

--- scripts/main.js
+++ scripts/main.js
@@ -6,14 +6,52 @@ import {UV} from 'uv'
 import {Vertices} from 'vertices';
 import {Normals} from 'normals';
 import {Material} from 'material';
-import {Plane} from 'plane';
+import {Plane} from 'planex';
 
 async function loadData() {
-    const responseF = await fetch('/scripts/shader/fragmentShader.frag');
-    const fragmentShader = await responseF.text();
+    const fragmentShader = `
+    #include <common>
 
-    const responseV = await fetch("/scripts/shader/vertexShader.vert");
-    const vertexShader = await responseV.text();
+uniform vec3 iResolution;
+uniform float iTime;
+uniform sampler2D iTexture;
+
+varying float height;
+varying vec2 vUV;
+
+// By iq: https://www.shadertoy.com/user/iq
+// license: Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.
+void mainImage( out vec4 fragColor, in vec2 fragCoord )
+{
+    // Normalized pixel coordinates (from 0 to 1)
+    vec2 uv = vUV;
+
+    vec4 colorTexture = texture2D(iTexture, uv);
+
+    fragColor = colorTexture;
+}
+void main() {
+    mainImage(gl_FragColor, gl_FragCoord.xy);
+}
+    `
+    const vertexShader = `
+    #define GLSLIFY 1
+// Common uniforms
+uniform float iTime;
+
+varying float height;
+varying vec2 vUV;
+
+/*
+ * The main program
+ */
+void main() {
+	height = cos(iTime+sqrt((position.x-100.0)*(position.x-100.0) + (position.z-100.0)*(position.z-100.0)));
+
+	vUV = uv;
+	gl_Position = projectionMatrix * modelViewMatrix * vec4(position + vec3(0,height,0), 1.0);
+}
+    `
 
     const simu = new SimulationScene(fragmentShader, vertexShader);
 }
